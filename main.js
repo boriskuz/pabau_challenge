a021f0ef0c341086cf7a023a37ebd39256bebaf6
Boris Kuzmanovski <boris.kuzmanovski1@gmail.com>
2024-03-21 22:46:29 +0100

    Finished challenge

--- main.js
+++ main.js
@@ -1,6 +1,62 @@
 "use strict";
-const path = [];
-const letters = [];
+const gridPathFinder = (grid) => {
+    const path = [];
+    const letters = [];
+    let row = 0;
+    let col = 0;
+    grid.forEach((arr, index) => {
+        const startPosition = arr.indexOf('>');
+        if (startPosition !== -1) {
+            row = index;
+            col = startPosition;
+        }
+    });
+    let prevCharPosition = [row, col];
+    while (grid[row][col] !== 's') {
+        const currentCharacter = grid[row][col];
+        path.push(currentCharacter);
+        if (/[A-Z]/.test(currentCharacter)) {
+            letters.push(currentCharacter);
+        }
+        if (currentCharacter !== '+') {
+            if (grid[row][col + 1] !== ' ' && col !== grid[row].length - 1 && col + 1 !== prevCharPosition[1]) {
+                prevCharPosition = [row, col];
+                col++;
+            }
+            else if (grid[row][col - 1] !== ' ' && col !== 0 && col - 1 !== prevCharPosition[1]) {
+                prevCharPosition = [row, col];
+                col--;
+            }
+            else if (grid[row + 1] != undefined && grid[row + 1][col] !== ' ' && row + 1 !== prevCharPosition[0] && row !== grid.length - 1) {
+                prevCharPosition = [row, col];
+                row++;
+            }
+            else if (grid[row - 1] != undefined && grid[row - 1][col] !== ' ' && row - 1 !== prevCharPosition[0]) {
+                prevCharPosition = [row, col];
+                row--;
+            }
+        }
+        else if (currentCharacter === '+') {
+            if (grid[row + 1] != undefined && grid[row + 1][col] !== ' ' && row + 1 !== prevCharPosition[0] && row !== grid.length - 1) {
+                prevCharPosition = [row, col];
+                row++;
+            }
+            else if (grid[row - 1] != undefined && grid[row - 1][col] !== ' ' && row > 0 && row - 1 !== prevCharPosition[0]) {
+                prevCharPosition = [row, col];
+                row--;
+            }
+            else if (grid[row][col - 1] !== ' ' && col !== 0 && col - 1 !== prevCharPosition[1]) {
+                prevCharPosition = [row, col];
+                col--;
+            }
+        }
+    }
+    path.push('s');
+    return {
+        path: path.join(''),
+        letters: letters.join(''),
+    };
+};
 const grid = [
     ['>', '-', '-', '-', 'A', '-', '@', '-', '+'],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
@@ -8,70 +64,10 @@ const grid = [
     ['|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'],
     ['s', ' ', ' ', ' ', 'C', '-', '-', '-', '+'],
 ];
-let row = 0;
-let col = 0;
-grid.forEach((arr, index) => {
-    const startPosition = arr.indexOf('>');
-    if (startPosition !== -1) {
-        row = index;
-        col = startPosition;
-    }
-});
-console.log(row, col);
-let previousCharacter = [row, col];
 grid.forEach((row) => {
     console.log(row.join(''));
 });
-while (grid[row][col] !== 's') {
-    const currentCharacter = grid[row][col];
-    path.push(currentCharacter);
-    if (/[A-Z]/.test(currentCharacter)) {
-        letters.push(currentCharacter);
-    }
-    debugger;
-    if (currentCharacter === '+') {
-        row++;
-    }
-    if (grid[row][col + 1] !== ' ' && col !== grid[row].length - 1) {
-        col++;
-    }
-    if (col === grid[row].length - 1 && row !== grid.length - 1) {
-        row++;
-    }
-    // if (grid[row + 1] !== undefined && grid[row + 1][col] !== ' ' && previousCharacter[0] == row && previousCharacter[1] !== col + 1) {
-    //   previousCharacter = [row, col];
-    //   row++;
-    // } else if (grid[row][col + 1] !== undefined && grid[row][col + 1] !== ' ' && previousCharacter[0] == row && previousCharacter[1] !== col + 1) {
-    //   previousCharacter = [row, col];
-    //   col++;
-    // } else if (grid[row - 1] !== undefined && grid[row - 1][col] !== ' ' && previousCharacter[0] !== row - 1 && previousCharacter[1] == col) {
-    //   previousCharacter = [row, col];
-    //   row--;
-    // } else if (grid[row][col - 1] !== undefined && grid[row][col - 1] !== ' ' && previousCharacter[0] == row && previousCharacter[1] !== col - 1) {
-    //   previousCharacter = [row, col];
-    //   col--;
-    // }
-}
-// if (currentCharacter === '+') {
-//   if (grid[row + 1] !== undefined && grid[row + 1][col] !== ' ' && previousCharacter !== grid[row + 1][col]) {
-//     previousCharacter = grid[row][col];
-//     row++;
-//   } else if (grid[row][col + 1] !== undefined && grid[row][col + 1] !== ' ' && previousCharacter !== grid[row][col + 1]) {
-//     previousCharacter = grid[row][col];
-//     col++;
-//   } else if (grid[row - 1] !== undefined && grid[row - 1][col] !== ' ' && previousCharacter !== grid[row - 1][col]) {
-//     previousCharacter = grid[row][col];
-//     row--;
-//   } else if (grid[row][col - 1] !== undefined && grid[row][col - 1] !== ' ' && previousCharacter !== grid[row][col - 1]) {
-//     previousCharacter = grid[row][col];
-//     col--;
-//   }
-// }
-// if (currentCharacter !== '+' && col !== grid[0].length - 1) {
-//   previousCharacter = grid[row][col];
-//   col++;
-// }
-// if (currentCharacter !== '+' && row !== grid.length - 1 && col === grid[0].length - 1 && grid[row][col] !== '+') {
-//   previousCharacter = grid[row][col];
-//   row++;
-// }
+console.log(' ');
+console.log('Path: ', gridPathFinder(grid).path);
+console.log(' ');
+console.log('Letters: ', gridPathFinder(grid).letters);
--- main.ts
+++ main.ts
@@ -1,5 +1,63 @@
-const path: string[] = [];
-const letters: string[] = [];
+const gridPathFinder = (grid: string[][]) => {
+  const path: string[] = [];
+  const letters: string[] = [];
+
+  let row = 0;
+  let col = 0;
+
+  grid.forEach((arr, index) => {
+    const startPosition = arr.indexOf('>');
+    if (startPosition !== -1) {
+      row = index;
+      col = startPosition;
+    }
+  });
+
+  let prevCharPosition: number[] = [row, col];
+
+  while (grid[row][col] !== 's') {
+    const currentCharacter = grid[row][col];
+    path.push(currentCharacter);
+
+    if (/[A-Z]/.test(currentCharacter)) {
+      letters.push(currentCharacter);
+    }
+
+    if (currentCharacter !== '+') {
+      if (grid[row][col + 1] !== ' ' && col !== grid[row].length - 1 && col + 1 !== prevCharPosition[1]) {
+        prevCharPosition = [row, col];
+        col++;
+      } else if (grid[row][col - 1] !== ' ' && col !== 0 && col - 1 !== prevCharPosition[1]) {
+        prevCharPosition = [row, col];
+        col--;
+      } else if (grid[row + 1] != undefined && grid[row + 1][col] !== ' ' && row + 1 !== prevCharPosition[0] && row !== grid.length - 1) {
+        prevCharPosition = [row, col];
+        row++;
+      } else if (grid[row - 1] != undefined && grid[row - 1][col] !== ' ' && row - 1 !== prevCharPosition[0]) {
+        prevCharPosition = [row, col];
+        row--;
+      }
+    } else if (currentCharacter === '+') {
+      if (grid[row + 1] != undefined && grid[row + 1][col] !== ' ' && row + 1 !== prevCharPosition[0] && row !== grid.length - 1) {
+        prevCharPosition = [row, col];
+        row++;
+      } else if (grid[row - 1] != undefined && grid[row - 1][col] !== ' ' && row > 0 && row - 1 !== prevCharPosition[0]) {
+        prevCharPosition = [row, col];
+        row--;
+      } else if (grid[row][col - 1] !== ' ' && col !== 0 && col - 1 !== prevCharPosition[1]) {
+        prevCharPosition = [row, col];
+        col--;
+      }
+    }
+  }
+
+  path.push('s');
+
+  return {
+    path: path.join(''),
+    letters: letters.join(''),
+  };
+};
 
 const grid = [
   ['>', '-', '-', '-', 'A', '-', '@', '-', '+'],
@@ -9,85 +67,14 @@ const grid = [
   ['s', ' ', ' ', ' ', 'C', '-', '-', '-', '+'],
 ];
 
-let row = 0;
-let col = 0;
-
-grid.forEach((arr, index) => {
-  const startPosition = arr.indexOf('>');
-  if (startPosition !== -1) {
-    row = index;
-    col = startPosition;
-  }
-});
-
-console.log(row, col);
-
-let previousCharacter: number[] = [row, col];
-
 grid.forEach((row) => {
   console.log(row.join(''));
 });
 
-while (grid[row][col] !== 's') {
-  const currentCharacter = grid[row][col];
+console.log(' ');
 
-  path.push(currentCharacter);
+console.log('Path: ', gridPathFinder(grid).path);
 
-  if (/[A-Z]/.test(currentCharacter)) {
-    letters.push(currentCharacter);
-  }
+console.log(' ');
 
-  debugger;
-
-  if (currentCharacter === '+') {
-    row++;
-  }
-
-  if (grid[row][col + 1] !== ' ' && col !== grid[row].length - 1) {
-    col++;
-  }
-
-  if (col === grid[row].length - 1 && row !== grid.length - 1) {
-    row++;
-  }
-
-  // if (grid[row + 1] !== undefined && grid[row + 1][col] !== ' ' && previousCharacter[0] == row && previousCharacter[1] !== col + 1) {
-  //   previousCharacter = [row, col];
-  //   row++;
-  // } else if (grid[row][col + 1] !== undefined && grid[row][col + 1] !== ' ' && previousCharacter[0] == row && previousCharacter[1] !== col + 1) {
-  //   previousCharacter = [row, col];
-  //   col++;
-  // } else if (grid[row - 1] !== undefined && grid[row - 1][col] !== ' ' && previousCharacter[0] !== row - 1 && previousCharacter[1] == col) {
-  //   previousCharacter = [row, col];
-  //   row--;
-  // } else if (grid[row][col - 1] !== undefined && grid[row][col - 1] !== ' ' && previousCharacter[0] == row && previousCharacter[1] !== col - 1) {
-  //   previousCharacter = [row, col];
-  //   col--;
-  // }
-}
-
-// if (currentCharacter === '+') {
-//   if (grid[row + 1] !== undefined && grid[row + 1][col] !== ' ' && previousCharacter !== grid[row + 1][col]) {
-//     previousCharacter = grid[row][col];
-//     row++;
-//   } else if (grid[row][col + 1] !== undefined && grid[row][col + 1] !== ' ' && previousCharacter !== grid[row][col + 1]) {
-//     previousCharacter = grid[row][col];
-//     col++;
-//   } else if (grid[row - 1] !== undefined && grid[row - 1][col] !== ' ' && previousCharacter !== grid[row - 1][col]) {
-//     previousCharacter = grid[row][col];
-//     row--;
-//   } else if (grid[row][col - 1] !== undefined && grid[row][col - 1] !== ' ' && previousCharacter !== grid[row][col - 1]) {
-//     previousCharacter = grid[row][col];
-//     col--;
-//   }
-// }
-
-// if (currentCharacter !== '+' && col !== grid[0].length - 1) {
-//   previousCharacter = grid[row][col];
-//   col++;
-// }
-
-// if (currentCharacter !== '+' && row !== grid.length - 1 && col === grid[0].length - 1 && grid[row][col] !== '+') {
-//   previousCharacter = grid[row][col];
-//   row++;
-// }
+console.log('Letters: ', gridPathFinder(grid).letters);
